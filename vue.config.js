7fa2af4c4b5df2a89158fb4d86d5b68febf08950
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2021-04-15 13:24:29 -0300

    fix: split chunks

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   configureWebpack: {
+    optimization: {
+      splitChunks: false
+    },
+
     resolve: {
       alias: {
         'vue-i18n': 'vue-i18n/dist/vue-i18n.runtime.esm-bundler.js'
